0889b7485deb623e6a4af89f8785d9902d1c7364
pulkit <>
2019-07-12 16:59:19 +0530

    initial commit

---
+++ MyJavaCodeProject/src/binarytree/Node.java
@@ -0,0 +1,14 @@
+package binarytree;
+
+public class Node {
+
+	public Node() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
